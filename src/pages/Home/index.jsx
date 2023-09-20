8ab4c526c37152126d5f215bedf17c9f196276c2
gabrielmesquita7 <xelectrozchannel@gmail.com>
2023-09-20 17:01:21 -0300

    remove search results

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from './home.module.css'
 import { Dropdown, Graphs } from "../../components";
-import { Header, JobResults } from "../../layout";
+import { Header} from "../../layout";
 
 const index = () => {
   return (
@@ -9,7 +9,6 @@ const index = () => {
       <Header />
       <Dropdown />
       <Graphs />
-      <JobResults />
     </div>
   )
 }
